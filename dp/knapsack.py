313370b4bfc838c095ff5072740fd1dc4c599fe4
gaurav <gauravmanmode@gmail.com>
2024-03-01 17:57:00 +0530

    knapsack

---
+++ dp/knapsack.py
@@ -0,0 +1,14 @@
+def knapsack(weights, values, capacity):
+    n = len(weights)
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for w in range(capacity + 1):
+            if weights[i - 1] <= w:
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
+            else:
+                dp[i][w] = dp[i - 1][w]
+    return dp[n][capacity]
+
+# Testing
+print(knapsack([1,3,4,5], [1,4,5,7], 7))
+print(knapsack([2,3,1,4], [4,5,3,7], 5))
