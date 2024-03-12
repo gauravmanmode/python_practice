29bc319842676d5f5932bcbd2435066b023a009a
gaurav <gauravmanmode@gmail.com>
2024-03-12 18:41:00 +0530

    fibonacci

---
+++ dp/fibonacci.py
@@ -0,0 +1,11 @@
+def fibonacci(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo)
+    return memo[n]
+
+# Testing
+print(fibonacci(5))
+print(fibonacci(10))
